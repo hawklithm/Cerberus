bf909dfc488c6b7bc444075157807358b9a6870c
hawklithm <woshipmx1991@sina.com>
2014-04-01 17:26:52 +0800

    machine info query change

--- src/main/java/com/hawklithm/cerberus/executor/ProcessInfoManagerExecutor.java
+++ src/main/java/com/hawklithm/cerberus/executor/ProcessInfoManagerExecutor.java
@@ -1,6 +1,7 @@
 package com.hawklithm.cerberus.executor;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,7 +9,7 @@ import org.apache.commons.lang.StringUtils;
 
 import com.hawklithm.cerberus.protocol.FrontEndingCommunicationProtocol;
 import com.hawklithm.cerberus.protocol.FrontEndingRequestCondition;
-import com.hawklithm.cerberus.protocol.OperateTypeConstant;
+import com.hawklithm.cerberus.protocol.MachineInfoOperator;
 import com.hawklithm.cerberus.protocol.ProtocolUtils;
 import com.multiagent.hawklithm.exception.NothingChangeAndDoNotNeedToExecuteException;
 import com.multiagent.hawklithm.leon.module.property.DO.ChangerAnnouncerPropertyArrayVersion;
@@ -17,16 +18,32 @@ import com.multiagent.hawklithm.leon.process.interface4rpc.RPCProcessInfoManager
 
 public class ProcessInfoManagerExecutor implements FrontEndingCommunicationExecutor{
 
+	private static final String RET_VALUE="retValue",PROCESS_NAME="processName",ID="id";
 	private RPCProcessInfoManagerInterface processInfoManager;
+	
+	private static void setRetValue(Object[] object,Map<String, Object> map){
+		map.put(RET_VALUE, object);
+	}
+	private static void  setProcessName(String name,Map<String, Object> map){
+		map.put(PROCESS_NAME, name);
+	}
+	private static void setId(Integer id,Map<String, Object> map){
+		map.put(ID, id);
+	}
 
 	@Override
 	public FrontEndingCommunicationProtocol<Map<String,Object>> execute(FrontEndingCommunicationProtocol<Map<String,Object>> message )
 			throws Exception {
 		FrontEndingCommunicationProtocol<Map<String,Object>> result = new FrontEndingCommunicationProtocol<Map<String,Object>>();
 		try {
-			 if (message.getOperateType().equals(OperateTypeConstant.OPERATE_QUERY)) {
+			 if (message.getOperateType().equals(MachineInfoOperator.OPERATE_QUERY)) {
 				query(message, result);
 				result.setStatusOk();
+			}else if (message.getOperateType().equals(MachineInfoOperator.MACHINE_DETAIL_QUERY)){
+				machineDetailQuery(message, result);
+				result.setStatusOk();
+			}else if (message.getOperateType().equals(MachineInfoOperator.MACHINE_DETAIL_SUBMIT)){
+				
 			}
 		} catch (Exception e) {
 			if (e instanceof NothingChangeAndDoNotNeedToExecuteException){
@@ -35,6 +52,47 @@ public class ProcessInfoManagerExecutor implements FrontEndingCommunicationExecu
 			e.printStackTrace();
 		}
 		return result;
+	}
+	
+	protected void machineDetailSubmit(FrontEndingCommunicationProtocol<Map<String, Object>> message) throws Exception{
+		if (message.getRows().size() > 1) {
+			throw new Exception("查询条件只能包含一个订单信息");
+		}
+		for (Map<String, Object> index:message.getRows()){
+			FrontEndingRequestCondition front=new FrontEndingRequestCondition(index);
+			ProcessInfoDO msg = getQueryCondition(front.getCondition());
+			if (StringUtils.isEmpty(msg.getProcessName())) continue;
+			Map<String, Object> ans= new HashMap<String, Object>();
+			if (msg.getId()!=null&&msg.getId().intValue()!=0){
+				ProcessInfoManagerExecutor.setRetValue(new String[]{processInfoManager.getEquipmentInfoById(msg.getProcessName(), msg.getId().intValue())}, ans);
+				ProcessInfoManagerExecutor.setId(msg.getId(), ans);
+			}else{
+				ProcessInfoManagerExecutor.setRetValue(processInfoManager.getProcessInfo(msg.getProcessName()), ans);
+			}
+			ProcessInfoManagerExecutor.setProcessName(msg.getProcessName(), ans);
+//			result.getRows().add(getFrontEndingRequest(ans).toMapping());
+		}
+	}
+	
+	protected void machineDetailQuery(FrontEndingCommunicationProtocol<Map<String, Object>> message,
+			FrontEndingCommunicationProtocol<Map<String, Object>> result) throws Exception{
+		if (message.getRows().size() > 1) {
+			throw new Exception("查询条件只能包含一个订单信息");
+		}
+		for (Map<String, Object> index:message.getRows()){
+			FrontEndingRequestCondition front=new FrontEndingRequestCondition(index);
+			ProcessInfoDO msg = getQueryCondition(front.getCondition());
+			if (StringUtils.isEmpty(msg.getProcessName())) continue;
+			Map<String, Object> ans= new HashMap<String, Object>();
+			if (msg.getId()!=null&&msg.getId().intValue()!=0){
+				ProcessInfoManagerExecutor.setRetValue(new String[]{processInfoManager.getEquipmentInfoById(msg.getProcessName(), msg.getId().intValue())}, ans);
+				ProcessInfoManagerExecutor.setId(msg.getId(), ans);
+			}else{
+				ProcessInfoManagerExecutor.setRetValue(processInfoManager.getProcessInfo(msg.getProcessName()), ans);
+			}
+			ProcessInfoManagerExecutor.setProcessName(msg.getProcessName(), ans);
+			result.getRows().add(getFrontEndingRequest(ans).toMapping());
+		}
 	}
 
 	protected void query(FrontEndingCommunicationProtocol<Map<String,Object>> message,
@@ -46,7 +104,7 @@ public class ProcessInfoManagerExecutor implements FrontEndingCommunicationExecu
 			FrontEndingRequestCondition front=new FrontEndingRequestCondition(index);
 			ProcessInfoDO msg = getQueryCondition(front.getCondition());
 			if (StringUtils.isEmpty(msg.getProcessName())) continue;
-			ProcessInfoDO ans=new  ProcessInfoDO();
+			Map<String, Object> ans=new  HashMap<String, Object>();
 			ChangerAnnouncerPropertyArrayVersion[] retValue=processInfoManager.getBufferedPropertyList(msg.getProcessName());
 			List<ChangerAnnouncerPropertyArrayVersion> checked=new ArrayList<ChangerAnnouncerPropertyArrayVersion>(retValue.length);
 			for (int i=0;i<retValue.length;i++){
@@ -56,13 +114,8 @@ public class ProcessInfoManagerExecutor implements FrontEndingCommunicationExecu
 			if (checked.size()==0){
 				throw new NothingChangeAndDoNotNeedToExecuteException("nothing change!");
 			}
-			ans.setRetValue(checked.toArray(new ChangerAnnouncerPropertyArrayVersion[checked.size()]));
-			ans.setProcessName(msg.getProcessName());
-//			if (msg.getId()!=null&&msg.getId().intValue()!=0){
-//				ans.setRetValue(new String[]{processInfoManager.getEquipmentInfoById(msg.getProcessName(), msg.getId().intValue())});
-//			}else{
-//				ans.setRetValue(processInfoManager.getProcessInfo(msg.getProcessName()));
-//			}
+			ProcessInfoManagerExecutor.setRetValue(checked.toArray(new ChangerAnnouncerPropertyArrayVersion[checked.size()]),ans);
+			ProcessInfoManagerExecutor.setProcessName(msg.getProcessName(),ans);
 			result.getRows().add(getFrontEndingRequest(ans).toMapping());
 		}
 	}
@@ -75,15 +128,13 @@ public class ProcessInfoManagerExecutor implements FrontEndingCommunicationExecu
 		return data;
 	}
 
-	private FrontEndingRequestCondition getFrontEndingRequest(ProcessInfoDO info) {
+	private FrontEndingRequestCondition getFrontEndingRequest(Map<String, Object> info) {
 		FrontEndingRequestCondition condition = new FrontEndingRequestCondition();
-		condition.getCondition().put("retValue", info.getRetValue());
-		condition.getCondition().put("processName", info.getProcessName());
+		condition.getCondition().put("retValue", info.get("retValue"));
+		condition.getCondition().put("processName", info.get("processName"));
+		condition.getCondition().put("id", info.get("id"));
 		return condition;
 	}
-
-
-
 
 	public RPCProcessInfoManagerInterface getProcessInfoManager() {
 		return processInfoManager;
---
+++ src/main/java/com/hawklithm/cerberus/protocol/MachineInfoOperator.java
@@ -0,0 +1,6 @@
+package com.hawklithm.cerberus.protocol;
+
+public class MachineInfoOperator extends OperateTypeConstant {
+	public static final String MACHINE_DETAIL_QUERY = "machine_detail_query",
+			MACHINE_DETAIL_SUBMIT = "machine_detail_submit";
+}
